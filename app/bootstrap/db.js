718ee4d6315b41ad37e338fd9c1f3e38bb5e0064
anwerjunaid <junaid.up.in@gmail.com>
2016-12-30 16:26:31 +0530

    With DB error catched

--- app/bootstrap/db.js
+++ app/bootstrap/db.js
@@ -4,12 +4,14 @@ module.exports = function db(config, callback){
 
     var connections = config.connections;
 
-    return _mmv.Promise.map(connections, function(connection){
-
-        return mongoose.createConnection(connection.string, connection.option, connection.config);
-
+    return _mmv.Promise.map(connections, function(connection, index){
+        var connection = mongoose.createConnection(connection.string, connection.option, connection.config);
+        connection.on('error', function(err){
+            console.log(err);
+        })
+        return connection;
     }).then(function(connecteds){
         _mmv.Connections = connecteds;
         callback();
-    });
+    })
 }
